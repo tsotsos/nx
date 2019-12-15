3babe1a73ece7f1d538681812434366d66b855cc
Georgios Tsotsos <tsotsos@gmail.com>
2019-12-15 17:16:27 +0200

    Initial Commit
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

---
+++ config.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+)
+
+type Config struct {
+	Protocol string
+	Host     string
+	Name     string
+	User     string
+	Pin      string
+}
+
+// New Config struct
+func NewConfig() *Config {
+	return &Config{
+		Protocol: getEnv("NX_PROTOCOL", ""),
+		Host:     getEnv("NX_HOST", ""),
+		Name:     getEnv("NX_NANE", ""),
+		User:     getEnv("NX_USER", ""),
+		Pin:      getEnv("NX_PIN", ""),
+	}
+}
+
+// returns Environment variable or default value
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
