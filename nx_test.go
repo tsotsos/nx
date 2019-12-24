9d38f9412af378e382802e1af857dbc60bc5f8ca
Georgios Tsotsos <tsotsos@gmail.com>
2019-12-25 01:55:51 +0200

    Nx testing for System status
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- nx_test.go
+++ nx_test.go
@@ -1,7 +1,9 @@
 package nx
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"reflect"
 	"testing"
 )
 
@@ -11,7 +13,21 @@ func init() {
 	}
 }
 
-func TestNx(t *testing.T) {
+func TestGetSystemStatus(t *testing.T) {
 	nx := NewNxAlarm()
-	nx.SetSystem(Chime)
+	data, err := nx.SystemStatus()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	v := reflect.ValueOf(data.System)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	typeOfS := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Printf("Field: %10s\tValue: %10v\n", typeOfS.Field(i).Name,
+			v.Field(i).Interface())
+	}
 }
